080a21a870c536c5198f9515ddb3e3c031f9a76d
Insup Jung <31408300+jis1218@users.noreply.github.com>
2018-08-07 22:27:56 +0900

    commit

--- main.c
+++ main.c
@@ -3,15 +3,23 @@
 
 int main()
 {
-    int *p = (int*)malloc(sizeof(int)); //int의 size만큼 메모리에 할당을 해줘야 한다.
+    int *q = (int*)malloc(sizeof(int)); //int의 size만큼 메모리에 할당을 해줘야 한다.
 
-    printf("size is %d", sizeof(int));
+    printf("size is %d\n", sizeof(int));
 
-    scanf("%d", p);
+    scanf("%d", q);
 
-    printf("%d", *p);
+    printf("%d", *q);
 
-    free(p);
+    free(q);
+
+    int a[3] = {10, 20, 30};
+    int *p = a; //a 자체가 주소
+    printf("a = %p, p = %p \n", a, p);
+    printf("%d %d %d \n", a[0], a[1], a[2]);
+    printf("%d %d %d \n", p[0], p[1], p[2]);
+    printf("%d %d %d \n", *a, *(a+1), *(a+2));
+    printf("%d %d %d \n", *p, *(p+1), *(p+2));
 
     return 0;
 }
